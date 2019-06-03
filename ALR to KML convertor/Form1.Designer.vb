890c09a1d73eec0b47b4bbac202c82a1f34f23b4
Marc Vandael <vandael.marc@gmail.com>
2019-06-03 14:17:02 +0200

    Archive

--- ALR to KML convertor/Form1.Designer.vb
+++ ALR to KML convertor/Form1.Designer.vb
@@ -24,25 +24,46 @@ Partial Class frmALRtoKML
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmALRtoKML))
         Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'RichTextBox1
         '
         Me.RichTextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(120, Byte), Integer), CType(CType(122, Byte), Integer), CType(CType(120, Byte), Integer))
         Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.RichTextBox1.Location = New System.Drawing.Point(-2, 211)
+        Me.RichTextBox1.Location = New System.Drawing.Point(0, 294)
         Me.RichTextBox1.Name = "RichTextBox1"
         Me.RichTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical
-        Me.RichTextBox1.Size = New System.Drawing.Size(252, 118)
+        Me.RichTextBox1.Size = New System.Drawing.Size(270, 62)
         Me.RichTextBox1.TabIndex = 1
         Me.RichTextBox1.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "AL3 to KML beta"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 32)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(244, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "No changepoints, just route. Drop files on this form"
         '
         'frmALRtoKML
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
-        Me.ClientSize = New System.Drawing.Size(249, 352)
+        Me.ClientSize = New System.Drawing.Size(264, 352)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.RichTextBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
@@ -53,8 +74,10 @@ Partial Class frmALRtoKML
         Me.Text = "ALR naar KML convertor"
         Me.TopMost = True
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
-
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
